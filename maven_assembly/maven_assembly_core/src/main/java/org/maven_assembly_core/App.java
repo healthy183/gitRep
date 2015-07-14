45c73000f7237969ae0037c58d18630e49ab6972
梁健康 <liangjiankang-it@bestpay.com.cn>
2015-07-14 11:48:16 +0800

    提交批量打包demo

---
+++ maven_assembly/maven_assembly_core/src/main/java/org/maven_assembly_core/App.java
@@ -0,0 +1,13 @@
+package org.maven_assembly_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		//throw new RuntimeException("异常来啦!"); 
+	}
+}
